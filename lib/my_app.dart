d5c53c9fae37048cc29f0cf156b5a665e0aeedcf
Leonardo Silva <leonardo.silva@simpaul.com.br>
2021-04-05 15:12:14 -0300

    config: add launcher screen to android

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,7 +9,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(),
+      home: Scaffold(),
     );
   }
 }
